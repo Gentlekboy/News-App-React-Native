f867fcae601fb44f5ad5c48a126daa7bc92d208e
gentlekboy <gentlekboy@gmail.com>
2022-09-13 12:04:28 +0100

    added the login screen component to the App component

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
+import LoginScreen from "./app/screens/authentication/login/LoginScreen";
 import SignUpScreen from "./app/screens/authentication/sign_up/SignUpScreen";
 import WelcomeScreen from "./app/screens/welcome/WelcomeScreen";
 
 export default function App() {
-  return <SignUpScreen />;
+  return <LoginScreen />;
 }
