46df70530ecc9b6d11b76da2cab9f7ab06474724
gentlekboy <gentlekboy@gmail.com>
2022-09-13 11:54:02 +0100

    created the sign up screen component

---
+++ app/screens/authentication/sign_up/styles.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "700",
+    marginBottom: 40,
+  },
+  input: {
+    backgroundColor: "#FFFFFF",
+    marginVertical: 10,
+    color: "#808191",
+    fontWeight: "400",
+  },
+  button: {
+    width: "100%",
+    marginTop: 40,
+    paddingVertical: 8,
+    borderRadius: 24,
+    backgroundColor: "#2596be",
+  },
+  notice: {
+    color: "#808191",
+  },
+  bold: {
+    fontWeight: "700",
+  },
+});
