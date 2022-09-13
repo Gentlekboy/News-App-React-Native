3bd6f0a1d9a5a532ee2221598601158b45d3d9f3
gentlekboy <gentlekboy@gmail.com>
2022-09-13 11:54:22 +0100

    added the sign up screen component to the App component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
+import SignUpScreen from "./app/screens/authentication/sign_up/SignUpScreen";
 import WelcomeScreen from "./app/screens/welcome/WelcomeScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <SignUpScreen />;
 }
